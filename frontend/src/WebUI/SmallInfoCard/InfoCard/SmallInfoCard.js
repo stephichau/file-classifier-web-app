706ee6a902ff8be83a708f9ad7582c6d29473cca
Stephanie Chau <schau@uc.cl>
2019-10-01 22:39:04 -0300

    ADded preliminary info cards

--- frontend/src/WebUI/SmallInfoCard/InfoCard/SmallInfoCard.js
+++ frontend/src/WebUI/SmallInfoCard/InfoCard/SmallInfoCard.js
@@ -2,20 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import classnames from 'classnames';
 
 const SmallInfoCard = ({
-
+  classes,
+  title,
+  subtitle,
+  children,
+  borderStyle,
 }) => (
-
+  <Paper
+    elevation={0}
+    className={classnames(classes.container, classes[borderStyle])}
+  >
+    {title && (
+      <div className={classnames(classes.textContainer, {
+        [classes.onlyTitle]: !subtitle,
+      })}>
+        <Typography>
+          {title}
+        </Typography>
+        {subtitle && (
+          <Typography>
+            {subtitle}
+          </Typography>
+        )}
+      </div>
+    )}
+    {children}
+  </Paper>
 );
 
 SmallInfoCard.defaultProps = {
+  title: undefined,
   subtitle: undefined,
+  children: undefined,
+  borderStyle: 'solid',
 };
 
 SmallInfoCard.propTypes = {
   classes: PropTypes.object.isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   subtitle: PropTypes.string,
+  children: PropTypes.node,
+  borderStyle: PropTypes.oneOf(['solid', 'dotted'])
 };
 
+export default SmallInfoCard;
