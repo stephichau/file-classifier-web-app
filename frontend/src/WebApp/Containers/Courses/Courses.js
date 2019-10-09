b5c3018ed5f6bb22237f537b3b967a446c8be158
Stephanie Chau <schau@uc.cl>
2019-10-09 17:23:08 -0300

    Added preliminary courses list

---
+++ frontend/src/WebApp/Containers/Courses/Courses.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SmallInfoCard from '../../../WebUI';
+
+const Courses = ({
+  classes,
+}) => {
+
+  return <div>Courses</div>;
+};
+
+Courses.defaultProps = {
+
+};
+
+Courses.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default Courses;
