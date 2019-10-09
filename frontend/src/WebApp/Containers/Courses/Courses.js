09add57b535ac4507bf6615273506e2bf89d439e
Stephanie Chau <schau@uc.cl>
2019-10-09 17:57:07 -0300

    Added container for courses

--- frontend/src/WebApp/Containers/Courses/Courses.js
+++ frontend/src/WebApp/Containers/Courses/Courses.js
@@ -1,20 +1,72 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SmallInfoCard from '../../../WebUI';
+import Typography from '@material-ui/core/Typography';
+import {
+  SmallInfoCard,
+  SkeletonSmallInfoCard,
+} from '../../../WebUI';
+
 
 const Courses = ({
   classes,
+  data,
+  loading,
+  i18n,
 }) => {
 
-  return <div>Courses</div>;
+  const onClick = () => {
+
+  };
+
+  const { courseCount, courses } = data;
+
+  return (
+    <div className={classes.container}>
+      <Typography>
+        {i18n.courses}
+      </Typography>
+      <div className={classes.cardContainer}>
+        {
+          !loading && courses && courses.map((course, index) => (
+            <div className={classes.card}>
+              <SmallInfoCard {...course} key={`Info--${index}`}>
+                <div className={classes.rectangle} />
+              </SmallInfoCard>
+            </div>
+          ))
+        }
+        {
+          loading && Array(courseCount).fill().map((_, index) => (
+            <div className={classes.card}>
+              <SkeletonSmallInfoCard title subtitle key={`Info--${index}`} />
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 };
 
 Courses.defaultProps = {
-
+  courses: undefined,
+  data: {
+    courseCount: 0,
+    courses: [],
+  },
+  loading: true,
+  i18n: {
+    courses: 'Courses',
+  },
 };
 
 Courses.propTypes = {
   classes: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    courseCount: PropTypes.number.isRequired,
+    courses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  }),
+  loading: PropTypes.bool,
+  i18n: PropTypes.object,
 };
 
 export default Courses;
--- frontend/src/WebApp/Containers/Courses/styles.js
+++ frontend/src/WebApp/Containers/Courses/styles.js
@@ -1,3 +1,21 @@
 export default theme => ({
-
+  container: {
+    minWith: 500,
+    maxWidth: 1300,
+    margin: '10px 20px',
+  },
+  cardContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  card: {
+    margin: '10px 10px 30px 0',
+  },
+  rectangle: {
+    width: '100%',
+    backgroundColor: 'blue',
+    height: '35%',
+    maxHeight: 220,
+    borderRadius: '0 0 4px 4px',
+  },
 });
