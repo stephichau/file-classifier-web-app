7a7fcafb215c5a26cbd79ea10ec42a09bccb6160
crabarca <caabarca1@uc.cl>
2019-10-14 16:47:46 -0300

    Added blank delete route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,6 +5,22 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .api import create_answer_sheet, get_all_answers, get_answer_by_id
 
 
+@app.route('/answers', methods=['GET'])
+def get_answers():
+  response = get_all_answers()
+  if response:
+    return response, 200
+  else:
+    return response, 500
+
+@app.route('/answers/<int:answer_id>', methods=['GET'])
+def get_answer(answer_id):
+  response = get_answer_by_id(12):
+  if response:
+    return response, 200
+  else:
+    return response, 500
+
 @app.route('/answer/create', methods=['POST'])
 def create_answer():
   # Call to sheet maker in order to create pdf
@@ -16,21 +32,9 @@ def create_answer():
   else:
     return response, 500 
 
-@app.route('/answers', methods=['GET'])
-def get_answers():
-  response = get_all_answers()
-  if response:
-    return response, 200
-  else:
-    response, 500
-
-@app.route('/answers/<int:answer>', methods=['GET'])
-def get_answer(answer_id):
-  response = get_answer_by_id(12):
-  if response:
-    return response, 200
-  else:
-    response, 500
+@app.route('/answer/<int:answer_id>', methods=['DELETE'])
+def delete_answer(answer_id)
+  pass
 
 
 
