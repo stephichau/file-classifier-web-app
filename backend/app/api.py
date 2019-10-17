49ed0478bbd87748415f085745457dd8a3a907ab
crabarca <caabarca1@uc.cl>
2019-10-17 12:51:20 -0300

    Added show and create courses

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,17 +3,18 @@ from mongoengine import *
 from .db import get_collection, create_fs_cursor
 from sheet_maker import main as make
 
+connect(os.getenv('MONGODB'))
 
 class Course(Document):
   name = StringField(max_length=200, required=True)
   semester = IntField(min_value = 0, max_value = 2, required=True)
   year = StringField(max_length=5, required=True)
-  professor = StringField(max_length=200, required=True)
+  instructor = StringField(max_length=200, required=True)
+  section = IntField(required=True)
 
 class Answer(Document):
   # delete all answer sheets when deleting the course
   course = ReferenceField(Course, reverse_delete_rule=CASCADE)
-
   upper_bound = IntField(min_value = 1, required = True)
   lower_bound = IntField(min_value = 0, required=True)
   evaluation = StringField(max_length=100, required=True)
@@ -21,13 +22,25 @@ class Answer(Document):
   answer_file = FileField(required=True)
 
 
-def create_course(data: dict) -> bool:
+def create_course_doc(data: dict) -> bool:
   _course = Course()
-  _course.name = str(data['name']).lower()
+  _course.name = str(data['course']).lower()
   _course.semester = int(data['semester'])
   _course.year = str(data['year']).lower()
-  _course.professor = str(data['professor'])
-  return True if _course.save() else return False
+  _course.instructor = str(data['instructor'])
+  _course.section = int(data['section'])
+  return True if _course.save() else False
+
+def get_course_doc(data: dict):
+  _course = Course.objects(
+    name = str(data['course']).lower(),
+    semester = int(data['semester']),
+    year = str(data['year']).lower(),
+    instructor = str(data['instructor']),
+    section = int(data['section'])
+  )
+  return _course.to_json() if _course else {}
+
 
 
 def create_answer_sheet(_data: dict) -> bool:
@@ -48,28 +61,12 @@ def create_answer_sheet(_data: dict) -> bool:
 
     return True
 
-def create_course(_data: dict) -> bool:
-  courses = get_collection('course')
-  data = {
-    'name': str(_data['name']).lower(),
-    'semester': str(_data['semester']).lower(),
-    'year': str(_data['year']).lower(),
-    ''
-  }
-
-  
-
 def get_all_answers():
-  cursor = get_collection('answer_sheet')
-  return cursor.find()
+  pass
   
 
 def get_answer_by_id(_id: int):
-  cursor = get_collection('answer_sheet')
-  sheet = cursor.find({'id': _id})
-  if sheet:
-    return sheet
-
+  pass
 
 
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,15 +1,22 @@
 from app import app
 from flask import render_template, jsonify, request
-from .api import create_answer_sheet, get_all_answers, get_answer_by_id, create_course
+from .api import create_answer_sheet, get_all_answers, get_answer_by_id, create_course_doc, get_course_doc
+
+@app.route('/', methods=['GET'])
+def index():
+  return 'Hello World'
 
 @app.route('/course', methods=['GET'])
-def get_courses():
-  pass
-
+def get_course():
+  _data = request.json
+  _course = get_course_doc(_data)
+  response = {'course': _course}
+  return jsonify(response), 200
+  
 @app.route('/course', methods=['POST'])
 def create_course():
   _data = request.json
-  _created = create_course(_data)
+  _created = create_course_doc(_data)
   response = {'success': _created}
   if _created:
     return response, 200
@@ -25,10 +32,7 @@ def get_answers():
 
 @app.route('/answers/<int:answer_id>', methods=['GET'])
 def get_answer(answer_id):
-  response = get_answer_by_id(12):
-  if response:
-    return response, 200
-  return response, 500
+  pass
 
 @app.route('/answer/create', methods=['POST'])
 def create_answer():
