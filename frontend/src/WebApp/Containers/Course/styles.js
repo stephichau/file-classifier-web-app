0e7aac9a47d8857e7e215ea573cf5e0daba434c1
Stephanie Chau <schau@uc.cl>
2019-10-19 20:48:58 -0300

    Changed border style

--- frontend/src/WebApp/Containers/Course/styles.js
+++ frontend/src/WebApp/Containers/Course/styles.js
@@ -9,6 +9,7 @@ export default theme => ({
     flexWrap: 'wrap',
   },
   card: {
+    borderRadius: 4,
     margin: '10px 10px 30px 0',
     '&:hover': {
       cursor: 'pointer',
@@ -21,11 +22,12 @@ export default theme => ({
     justifyContent: 'center',
     alignItems: 'center',
     fontSize: 22,
+    borderRadius: 4,
     '&:hover': {
       border: '1.5px solid orange',
       borderRadius: 4,
-      width: '100.5%',
-      height: '101%',
+      width: '100%',
+      height: '100%',
     },
   },
 });
