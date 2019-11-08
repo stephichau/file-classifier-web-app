976fd06a94d4365bc232f5a664960f4113dc319b
crabarca <caabarca1@uc.cl>
2019-11-08 20:59:42 -0300

    Organized folders in a more smart way

--- backend/app/lib/utils/file_merger.py
+++
@@ -1,6 +0,0 @@
-import os
-from .log import progress
-
-def pdf_merger(_img_list: list, _path_name: str) -> None:
-    _img_list[0].save(_path_name, 'PDF', resolution=20,
-        save_all=True, append_images=progress(_img_list[1:]))
