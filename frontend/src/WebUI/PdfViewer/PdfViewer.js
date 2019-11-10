75258e7c9d7023518307fe496a2f5ba97f1276f9
Stephanie Chau <schau@uc.cl>
2019-11-10 15:59:25 -0300

    Added PdfViewer component

---
+++ frontend/src/WebUI/PdfViewer/PdfViewer.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Document, Page } from 'react-pdf'
+
+const PdfViewer = ({
+  classes,
+  url,
+  i18n,
+  ...restOfProps
+}) => {
+  const [state, setState] = useState({
+    numPages: null,
+    pageNumber: 1,
+  });
+
+  const [downloading, setDownloading] = useState(false);
+
+  const onLoadSuccess = ({ numPages }) => {
+    setState({ ...state, numPages });
+  };
+
+  const onLoadError = ({ message }) => {
+    console.log(message);
+  };
+
+  const onPrevPage = () => {
+    setState({
+      ...state,
+      pageNumber: state.pageNumber - 1 >= 0 ? state.pageNumber - 1 : 1,
+    });
+  };
+
+  const onNextPage = () => {
+    setState({
+      ...state,
+      pageNumber: state.pageNumber + 1,
+    });
+  };
+
+  return (
+    <div style={{ margin: '0 auto'}}>
+      <div className={classes.buttonsContainer}>
+        <div className={classes.pagesContainer}>
+          <Button
+            className={classes.button}
+            onClick={onPrevPage}
+            variant="outlined"
+          >
+            <Typography variant="caption">
+              {i18n.previous}
+            </Typography>
+          </Button>
+          <Typography
+            className={classes.pageNumber}
+            variant="caption"
+          >
+            {state.pageNumber} / {state.numPages || i18n.notAvailable}
+          </Typography>
+          <Button
+            className={classes.button}
+            onClick={onNextPage}
+            variant="outlined"
+          >
+            <Typography variant="caption">
+              {i18n.next}
+            </Typography>
+          </Button>
+        </div>
+        <Button
+          className={classes.button}
+          variant="outlined"
+        >
+          <a href={url} target="_blank" download>
+            <Typography variant="caption">
+              {i18n.download}
+            </Typography>
+          </a>
+        </Button>
+      </div>
+      <div className={classes.documentContainer}>
+        <Document
+          file={url}
+          onLoadSuccess={onLoadSuccess}
+          onLoadError={onLoadError}
+        >
+          <Page pageNumber={state.pageNumber} width={600} />
+        </Document>
+      </div>
+    </div>
+  );
+};
+
+PdfViewer.defaultProps = {
+  url: 'https://cass-upload.oss-cn-shenzhen.aliyuncs.com/test/1572231351651_ime88888_ENTERING.pdf',
+  i18n: {
+    previous: 'Prev',
+    next: 'Next',
+    download: 'Descargar',
+    notAvailable: 'n/a',
+  }
+};
+
+PdfViewer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  url: PropTypes.string,
+  i18n: PropTypes.shape({
+    previous: PropTypes.string.isRequired,
+    next: PropTypes.string.isRequired,
+    download: PropTypes.string.isRequired,
+    notAvailable: PropTypes.string,
+  }),
+};
+
+export default PdfViewer;
---
+++ frontend/src/WebUI/PdfViewer/index.js
@@ -0,0 +1,6 @@
+import { withStyles } from '@material-ui/core';
+import styles from './styles';
+
+import PdfViewer from './PdfViewer';
+
+export default withStyles(styles)(PdfViewer);
